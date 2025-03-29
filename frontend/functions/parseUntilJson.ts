5e575057dafa04c470d5fa98dd85b5a030d8a601
asengupta07 <arnabsengupta104@gmail.com>
2025-03-30 00:10:12 +0530

    WE dont log CONSOLES

--- frontend/functions/parseUntilJson.ts
+++ frontend/functions/parseUntilJson.ts
@@ -11,29 +11,14 @@ export function parseUntilJson(jsonstr: string): Record<string, any> {
   }
   try {
     const properlyParsedJson = JSON.parse(jsonRes);
-    console.log(
-      '\n\n===============================\nJSON parsed properly\n===============================\n\n'
-    );
     if (typeof properlyParsedJson === 'object' && properlyParsedJson !== null) {
       return properlyParsedJson;
     } else {
       jsonRes = properlyParsedJson;
     }
   } catch (error) {
-    console.error(
-      '\n\n===============================\nError parsing the JSON using regular JSON.parse...\n===============================\n'
-    );
     console.error(error);
   }
-
-  console.log(
-    '\n=================\njsonRes type:\n=================\n',
-    typeof jsonRes
-  );
-  if (typeof jsonRes === 'object') {
-    console.log(Object.keys(jsonRes));
-  }
-  console.log('\n=================\njsonRes:\n=================\n', jsonRes);
   const curlIndex =
     jsonRes.indexOf('{') === -1 ? jsonRes.length : jsonRes.indexOf('{');
   const sqIndex =
@@ -46,21 +31,14 @@ export function parseUntilJson(jsonstr: string): Record<string, any> {
   if (jsonRes.startsWith('`') || jsonRes.endsWith('`')) {
     jsonRes = jsonRes.replaceAll('```', '');
   }
-
-  console.log('Filtered JSON res = ', jsonRes);
   jsonRes = jsonRes.replaceAll('\n', '\\n');
   jsonRes = jsonRes.replaceAll('{\\n', '{').replaceAll('\\n}', '}');
-  console.log(jsonRes);
   try {
     while (typeof jsonRes === 'string') {
       jsonRes = parseJSON(jsonRes, ALL);
     }
-    console.log('\n\nParsed JSON using partial JSON parser = \n\n', jsonRes);
     return jsonRes;
   } catch (error) {
-    console.error(
-      '\n\n===============================\nError parsing the JSON...\n===============================\n'
-    );
     console.error(error);
     return {};
   }
